f56471a89ece3616a95ffdcc5a1090abfdc7686b
Philipp Gorczak <p.gorczak@gmail.com>
2016-05-11 14:58:24 +0200

    Require ros should ensure ros is installed

--- manifests/package.pp
+++ manifests/package.pp
@@ -1,6 +1,6 @@
 define ros::package ($ensure='installed', $require=[]) {
-  include ros
-  include ros::params
+  require ros
+  require ros::params
   package { "ros-${ros::params::ros_version}-${title}":
     ensure  => $ensure,
     require => [Class['ros']] + $require
